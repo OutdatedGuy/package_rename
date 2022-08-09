26b5d0fbf39a105e4ddcd10e9b38429cba86eb99
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-08-09 13:40:36 +0530

    FEAT: Created 'set' for binary entry point

---
+++ bin/set.dart
@@ -0,0 +1,4 @@
+import 'package:package_rename/package_rename.dart' as package_rename;
+
+/// Command line entry point to set package details.
+void main(List<String> arguments) => package_rename.set(arguments);
