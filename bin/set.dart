6b9adcf21259391d41bb6c2c8f63633516ee9fe5
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-08-17 18:00:13 +0530

    DOCS: added exec command for set bin fn in comment

--- bin/set.dart
+++ bin/set.dart
@@ -1,4 +1,9 @@
 import 'package:package_rename/package_rename.dart' as package_rename;
 
-/// Command line entry point to set package details.
+/// Command line entry point to configure package details.
+///
+/// Execute following command to set package details:
+/// ```bash
+/// flutter pub run package_rename:set
+/// ```
 void main(List<String> arguments) => package_rename.set(arguments);
