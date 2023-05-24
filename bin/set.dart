62e77bd329ebe611f8358c400d9cbb3ffa7fe370
OutdatedGuy <everythingoutdated@gmail.com>
2023-05-24 08:14:26 +0530

    feat: add package_rename binary to simplify command
    Marked package_rename:set as deprecated

--- bin/set.dart
+++ bin/set.dart
@@ -1,4 +1,8 @@
-import 'package:package_rename/package_rename.dart' as package_rename;
+// Third Part Packages
+import 'package:logger/logger.dart';
+
+// Binaries
+import 'package_rename.dart' as package_rename;
 
 /// Command line entry point to configure package details.
 ///
@@ -6,4 +10,18 @@ import 'package:package_rename/package_rename.dart' as package_rename;
 /// ```bash
 /// flutter pub run package_rename:set
 /// ```
-void main(List<String> arguments) => package_rename.set(arguments);
+void main(List<String> arguments) {
+  Logger(
+    filter: ProductionFilter(),
+    printer: PrettyPrinter(
+      lineLength: 80,
+      methodCount: 0,
+      noBoxingByDefault: true,
+      printEmojis: false,
+    ),
+  ).w(
+    'This command is deprecated and replaced with '
+    '"flutter pub run package_rename"',
+  );
+  package_rename.main(arguments);
+}
